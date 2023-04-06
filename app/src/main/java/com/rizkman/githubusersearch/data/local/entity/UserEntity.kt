92a3e7c6c85619ffd1cce292cfb42bf518119efd
mansyaahh <120728794+mansyaahh@users.noreply.github.com>
2023-04-06 16:00:34 +0800

    Adding entity and dependencies

--- app/src/main/java/com/rizkman/githubusersearch/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/rizkman/githubusersearch/data/local/entity/UserEntity.kt
@@ -1,4 +1,12 @@
 package com.rizkman.githubusersearch.data.local.entity
 
-class UserEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class UserEntity (
+    @PrimaryKey(autoGenerate = false)
+    var username: String = "",
+    var avatarUrl: String? = null,
+        )
+
