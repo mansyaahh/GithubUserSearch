68e4d4692f033cb5d90ddcb81e8f62b3c5050d32
mansyaahh <120728794+mansyaahh@users.noreply.github.com>
2023-04-06 16:10:44 +0800

    Adding Parcelize

--- app/src/main/java/com/rizkman/githubusersearch/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/rizkman/githubusersearch/data/local/entity/UserEntity.kt
@@ -1,12 +1,15 @@
 package com.rizkman.githubusersearch.data.local.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity
+@Parcelize
 data class UserEntity (
     @PrimaryKey(autoGenerate = false)
     var username: String = "",
     var avatarUrl: String? = null,
-        )
+        ): Parcelable
 
